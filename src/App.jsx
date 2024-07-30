05f3dc8bfd787a96cf2d729360ae29ac17477edc
ditinagrawal <ditin.agrawal05@gmail.com>
2024-07-30 13:29:13 +0530

    Add chat feature

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { socket } from "./socket";
+
+const App = () => {
+    const [value, setValue] = React.useState("");
+    const [id, setId] = React.useState("");
+    const [messages, setMessages] = React.useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        socket.emit("send_msg", value);
+    };
+    useEffect(() => {
+        socket.on("connect", () => {
+            setId(socket.id);
+        });
+        socket.on("send_msg", (data) => {
+            setMessages((prev) => [...prev, data]);
+        });
+        // socket.emit("send_msg", "Hello from client!");
+        // socket.on("disconnect", () => {
+        //     console.log("Disconnected from server");
+        // });
+    }, []);
+    return (
+        <div className="h-screen bg-[#202021]">
+            <div>
+                {messages.map((msg, i) => (
+                    <div key={i}>
+                        <span>{msg}</span>
+                    </div>
+                ))}
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <button type="submit">send</button>
+            </form>
+        </div>
+    );
+};
+
+export default App;
